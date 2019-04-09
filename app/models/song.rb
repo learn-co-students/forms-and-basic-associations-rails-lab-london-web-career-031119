8d0f036c8b692ff8c8beaaf8fd2a43b0d4b8cc74
peter-keller <keller.f.peter@gmail.com>
2019-04-09 22:03:42 +0100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -25,7 +25,7 @@ class Song < ActiveRecord::Base
 
   def note_contents=(notes)
     notes.each do |note|
-      self.notes.build(content: note) unless note.empty?
+      self.notes << Note.create(content: note) unless note.empty?
     end
     # self.notes.build(content: notes.reject(&:empty?))
   end
